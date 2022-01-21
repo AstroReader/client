3b4a1b35305c472f170c834dfb613132e2a5397d
John Dinh <johnduydinh@gmail.com>
2022-01-21 15:51:47 -0500

    initial login screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,41 @@
-function App() {
-  return <div>Hello World</div>;
-}
+import { useEffect, useState, useRef } from "react";
+import { Outlet } from "react-router-dom";
+import logoSvg from "./assets/icons/logo.svg";
+const App = () => {
+  const [mobileHeight, setMobileHeight] = useState(window.innerHeight);
+  return (
+    <div id="background" className="bg-dark min-h-screen w-full">
+      <div className="pb-20">
+        <div className="text-center py-20 md:py-10 xl:py-20">
+          <img className="inline w-24" src={logoSvg} alt="" />
+          <h1 className="inline text-white text-5xl xl:text-7xl align-middle">
+            stro Reader
+          </h1>
+        </div>
+        <form className="py-12 px-9 mx-auto lg:h-128 bg-black/50 w-9/12 lg:w-128 rounded-md flex justify-center flex-col">
+          <div>
+            <label className="text-2xl text-white">
+              Username
+              <input
+                className="mt-4 mb-6 lg:mb-12 px-4 block w-full bg-gray/50 h-16 rounded-lg outline-none text-2xl"
+                type="text"
+              />
+            </label>
+            <label className="text-2xl text-white">
+              Password
+              <input
+                className="mt-4 mb-12 lg:mb-12 px-4 block w-full bg-gray/50 h-16 rounded-lg outline-none text-2xl"
+                type="password"
+              />
+            </label>
+            <button className="px-4 block w-full bg-gradient-to-r from-btnGradient1 via-btnGradient2/80 to-btnGradient3 h-16 rounded-lg outline-none text-2xl font-bold text-white">
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#353245",
+        gray: "#595959",
+        btnGradient1: "#5C6ECC",
+        btnGradient2: "#DE71ED",
+        btnGradient3: "#FF7575",
+      },
+      height: {
+        128: "32rem",
+      },
+      width: {
+        128: "32rem",
+      },
+    },
+  },
+  plugins: [],
+};
