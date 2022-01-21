a7d1b0e0e3fe064f09b63ca2c36392de555525b4
John Dinh <johnduydinh@gmail.com>
2022-01-21 16:23:16 -0500

    format tailwindcss class names

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,38 @@
-import { useEffect, useState, useRef } from "react";
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import logoSvg from "./assets/icons/logo.svg";
 const App = () => {
-  const [mobileHeight, setMobileHeight] = useState(window.innerHeight);
+  const initialFormData = {
+    username: "",
+    password: "",
+  };
+  const [formData, setFormData] = useState({});
   return (
-    <div id="background" className="bg-dark min-h-screen w-full">
+    <div id="background" className="w-full min-h-screen bg-dark">
       <div className="pb-20">
-        <div className="text-center py-20 md:py-10 xl:py-20">
+        <div className="py-20 text-center md:py-10 xl:py-20">
           <img className="inline w-24" src={logoSvg} alt="" />
-          <h1 className="inline text-white text-5xl xl:text-7xl align-middle">
+          <h1 className="inline text-5xl text-white align-middle xl:text-7xl">
             stro Reader
           </h1>
         </div>
-        <form className="py-12 px-9 mx-auto lg:h-128 bg-black/50 w-9/12 lg:w-128 rounded-md flex justify-center flex-col">
+        <form className="flex flex-col justify-center w-9/12 py-12 mx-auto px-9 lg:h-128 bg-black/50 lg:w-128 rounded-md">
           <div>
             <label className="text-2xl text-white">
               Username
               <input
-                className="mt-4 mb-6 lg:mb-12 px-4 block w-full bg-gray/50 h-16 rounded-lg outline-none text-2xl"
+                className="block w-full h-16 px-4 mt-4 mb-6 text-2xl rounded-lg outline-none lg:mb-12 bg-gray/50"
                 type="text"
               />
             </label>
             <label className="text-2xl text-white">
               Password
               <input
-                className="mt-4 mb-12 lg:mb-12 px-4 block w-full bg-gray/50 h-16 rounded-lg outline-none text-2xl"
+                className="block w-full h-16 px-4 mt-4 mb-12 text-2xl rounded-lg outline-none lg:mb-12 bg-gray/50"
                 type="password"
               />
             </label>
-            <button className="px-4 block w-full bg-gradient-to-r from-btnGradient1 via-btnGradient2/80 to-btnGradient3 h-16 rounded-lg outline-none text-2xl font-bold text-white">
+            <button className="block w-full h-16 px-4 text-2xl font-bold text-white rounded-lg outline-none bg-gradient-to-r from-btnGradient1 via-btnGradient2/80 to-btnGradient3">
               Login
             </button>
           </div>
